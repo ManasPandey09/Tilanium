ed33bd7672143ee5352b4a6a2c91722fb9f51f1c
ManasPandey09 <manaspandey2709@gmail.com>
2025-09-03 00:36:30 +0530

    flipping and running

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,14 +7,18 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float runSpeed = 5f;
     Vector2 moveInput;
     Rigidbody2D rb;
+
+    Animator myAnimator;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     void Update()
     {
         Run();
+        FlipPlayer();
     }
 
     void OnMove(InputValue value)
@@ -26,5 +31,17 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rb.linearVelocityY);
         rb.linearVelocity = playerVelocity;
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb.linearVelocityX) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+    }
+
+    void FlipPlayer()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb.linearVelocityX) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rb.linearVelocityX), 1f);
+        }
     }
 }
